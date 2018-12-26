de95d95f44ff770c0ea2cd64ddb112309545699e
vladgmail <cevchris23@gmail.com>
2018-12-26 00:16:34 -0500

    Children Properties or props.id

---
+++ counter-store/src/Components/Counter1.jsx
@@ -0,0 +1,91 @@
+//using Class library
+import React, { Component } from "react";
+
+import "../Sheets/Sheets.css";
+
+//ClassJSX-extends component
+class Countero extends Component {
+  //No.1 ======= Les champs propriétés de la classe Modèle. ==========================
+  state = {
+    //pass data
+       value: this.props.value 
+  };
+
+  /*  constructor(){
+    super();
+    this.handleBtnIncrement = this.handleBtnIncrement.bind(this);
+  } */
+
+  //No.2 ======Special delivery method - that report to the Dom.=========================
+  // necessary method-1
+  render() {
+    //delivery may take some local property_variables here that will be reporting to the Dom.
+    //create provisory local_style back-CSS
+ 
+   //console.log("showTheProps", this.props);
+    //report what you want below the return:
+    //{ this.props.children} or  { this.props.id}  
+    return (
+      <div className="boxit">
+
+        <h4> Item { this.props.id} </h4>
+      
+        <span className={this.colorBadgeClasses()}> {this.formatCount()}</span>
+        <button
+         // onClick={this.handleBtnIncrement}
+         // onClick={this.doHandleInrement}
+          onClick={ () =>this.handleBtnIncrement()}
+
+          //learn to do it this way
+         // onClick={ () =>this.handleBtnIncrement(product)}
+
+          className="btn  btn-secondary btn-sm"
+        > Click to Increment </button>
+      
+       
+      </div>
+    ); //end report
+
+  } //end delivery 
+//=============================//Create all functions or methods after render\\==========================================================================
+  
+ //function-5
+ handleBtnIncrement = () => {
+  // console.log("increment clicked", this);
+ // console.log(product);
+
+   this.setState({ count: this.state.value  += 1  });
+ };
+
+  //function-5.1
+  /*
+  doHandleInrement =()=>{
+    this.handleBtnIncrement({ id: 1});
+  };
+*/
+
+
+  //No.3 ============= Some other methods to get control==========================
+ 
+ 
+  //method-3
+  formatCount() {
+    return this.state.count;
+  }
+
+  //method-4 CLASSIC_condition to get changed effect color on badgeText
+  colorBadgeClasses() {
+    let color_badge = "badge m-2 badge-";  
+    color_badge += this.state.value === 0 ? "warning" : "primary";
+    return color_badge;
+  }
+ 
+  //method-6
+  otherNewArrowMethod = () => {};
+  //method-7
+  usedOldMethod() {}
+
+} //end class
+//Finally class available to use(importable class in index.js).
+export default Countero;
+//========================******==============================
--- counter-store/src/Components/Counters.jsx
+++ counter-store/src/Components/Counters.jsx
@@ -1,30 +1,68 @@
 //imrc tab
 import React, { Component } from 'react';
-import "../Sheets/Sheets.css";
-import Countero from './Countera'
+import "./sheet.css";
+import Countero from './Counter1'
 
 
 //ClassJSX-extends component : cc tab
 class Counters extends Component {
-    state = { img1Src: "http://localhost/photos/200.jpg" }
 
-    //delivery
+    //state-declaration
+    state = { img1Src: "http://localhost/photos/200.jpg" ,
+    products:[
+        { id: 1, value:0},
+        { id: 2, value:20},
+        { id: 3, value:30},
+        { id: 4, value:40}
+    ]
+    
+    }
+    //delivery method
     render() { 
+        //report-return the import class 
+        //or call whatever function you want below the report-return:
+        //composing component Countero de Counter1 //map properties with keyID
+        //Children
+        // <h4>Item # {item.id}</h4> or id={item.id}
+        return (
+           <div className="boxit">       
+                <img src={this.state.img1Src}
+                className="boxit" 
+                alt="JlovSmile"
+                height="15%"
+                width="15%"
+                /> 
+                
+                {this.rendRecaps()}  
+               
+                { this.state.products.map(item => 
+                <Countero key={item.id} value={item.value} id={item.id}> </Countero>)}
+                
+                    
+                
+            </div>  
 
-        //report what you want below the return:
-        return (<div className="boxit">
-       
-        <img src={this.state.img1Src}
-          alt="JloSmile"
-          height="15%"
-          width="15%"
-          className="boxit"  />    
-        </div>  );
-
+        );//end report
     } // end delivery
+
+//---------------------- functions ------------------------------------
+    rendRecaps() {
+  
+        return (
+          <ul>
+            <li> {"Recap # 2: Composing Components "}</li>
+            <ol>
+            <li> {'Pass Data'}</li>
+            <li> {'Raise and handle Events '}</li>
+            <li> {"Multiple Components in Sync "}</li>
+            <li> {"Functional Components "}</li>
+            <li> {"Lifecycle Hooks "}</li>
+            </ol>
+    
+          </ul>
+        );
+    } //end of rendRecaps
 }//end class
-
-
 //must export to work
 export default Counters;
 
